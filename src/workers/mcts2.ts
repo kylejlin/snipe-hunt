269922ad33488f4568306ec9326a35646144111d
Kyle Lin <kylejlin.us@gmail.com>
2020-06-09 16:28:55 -0700

    Write second draft of MCTS worker

---
+++ src/workers/mcts2.ts
@@ -0,0 +1,111 @@
+import { option, Option } from "rusty-ts";
+import { getMctsAnalyzerIfStateIsNonTerminal, MctsAnalyzerV2 } from "../mcts2";
+import {
+  MctsWorkerMessage,
+  UpdateMctsAnalysisNotification,
+  UpdateMctsAnalyzerGameStateRequest,
+  WorkerMessageType,
+} from "../types";
+
+export {};
+
+const ROLLOUT_BATCH_SIZE = 5000;
+const UNCERTAINTY_THRESHOLD = 1e-3;
+const MAX_MEAN_VALUE_BEFORE_DECLARING_VICTORY = 1 - UNCERTAINTY_THRESHOLD;
+const MIN_MEAN_VALUE_BEFORE_DECLARING_DEFEAT = UNCERTAINTY_THRESHOLD;
+const MIN_ROLLOUTS_NEEDED_TO_DECLARE_STATE_TERMINAL = 1e6;
+const MIN_MILLISECONDS_BETWEEN_POSTS = 0.2e3;
+
+declare const self: Worker;
+
+let optMctsAnalyzer: Option<MctsAnalyzerV2> = option.none();
+let lastPosted = 0;
+
+self.addEventListener("message", (e) => {
+  const data = e.data;
+  if ("object" === typeof data && data !== null) {
+    {
+      if (data.x) {
+        self.postMessage({
+          messageType: WorkerMessageType.LogNotification,
+          data: optMctsAnalyzer.unwrap().getRootSummary(),
+        });
+      }
+    }
+    const message: MctsWorkerMessage = data;
+    switch (message.messageType) {
+      case WorkerMessageType.UpdateMctsAnalyzerGameStateRequest:
+        onGameStateUpdateRequest(message);
+        break;
+    }
+  }
+});
+
+function onGameStateUpdateRequest(
+  message: UpdateMctsAnalyzerGameStateRequest
+): void {
+  const newGameState = message.gameState;
+
+  optMctsAnalyzer = getMctsAnalyzerIfStateIsNonTerminal(
+    newGameState,
+    newGameState.plies.length + 3,
+    22 * 5e6
+  );
+}
+
+function analysisUpdateLoop() {
+  optMctsAnalyzer.match({
+    none: () => {
+      const message: UpdateMctsAnalysisNotification = {
+        messageType: WorkerMessageType.UpdateMctsAnalysisNotification,
+        optAnalysis: null,
+      };
+      self.postMessage(message);
+    },
+
+    some: (analyzer) => {
+      const root = analyzer.getRootSummary();
+      const meanValue = root.value / root.rollouts;
+      const isTerminal =
+        root.rollouts >= MIN_ROLLOUTS_NEEDED_TO_DECLARE_STATE_TERMINAL &&
+        (meanValue > MAX_MEAN_VALUE_BEFORE_DECLARING_VICTORY ||
+          meanValue < MIN_MEAN_VALUE_BEFORE_DECLARING_DEFEAT);
+
+      if (!isTerminal) {
+        for (let i = 0; i < ROLLOUT_BATCH_SIZE; i++) {
+          analyzer.performRollout();
+        }
+      }
+
+      const now = Date.now();
+      const shouldPost = now > lastPosted + MIN_MILLISECONDS_BETWEEN_POSTS;
+      if (shouldPost) {
+        postAnalysisUpdate(analyzer);
+        lastPosted = now;
+      }
+    },
+  });
+
+  requestAnimationFrame(analysisUpdateLoop);
+}
+
+function postAnalysisUpdate(analyzer: MctsAnalyzerV2): void {
+  const root = analyzer.getRootSummary();
+  const bestAtomic = analyzer.getBestAtomic();
+  const childWithBestAtomic = analyzer.getSummaryOfChildWithBestAtomic();
+
+  const message: UpdateMctsAnalysisNotification = {
+    messageType: WorkerMessageType.UpdateMctsAnalysisNotification,
+    optAnalysis: {
+      currentStateValue: root.value,
+      currentStateRollouts: root.rollouts,
+
+      bestAtomic,
+      bestAtomicValue: childWithBestAtomic.value,
+      bestAtomicRollouts: childWithBestAtomic.rollouts,
+    },
+  };
+  self.postMessage(message);
+}
+
+analysisUpdateLoop();
